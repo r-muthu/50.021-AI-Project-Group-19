cda5dd94be5f50bcb25cdab5a775e8fcdcc7d582
r-muthu <muthuramaswamy2003@gmail.com>
2025-04-12 12:43:46 +0800

    changes to files

--- models.py
+++ models.py
@@ -10,8 +10,17 @@ class BaseModel(nn.Module):
             hidden_dropout_prob=hidden_dropout_prob
         )
 
-    def forward(self, *args, **kwargs):
-        return self.pretrained_model(*args, **kwargs)
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
+        return self.pretrained_model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            labels=labels,
+            return_dict=True
+        )
+
+    def resize_token_embeddings(self, token_len):
+        self.pretrained_model.resize_token_embeddings(token_len)
 
 class CustomClassifier(nn.Module):
     def __init__(self, model_checkpoint, num_labels=2, hidden_dropout_prob=0.1):
@@ -28,6 +37,9 @@ class CustomClassifier(nn.Module):
             nn.Dropout(hidden_dropout_prob),
             nn.Linear(hidden_size, num_labels)  # Binary classification (0 or 1)
         )
+
+    def resize_token_embeddings(self, token_len):
+        self.pretrained_model.resize_token_embeddings(token_len)
 
     def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
         outputs = self.pretrained_model(
